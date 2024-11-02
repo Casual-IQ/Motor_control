cf8139533aa7807298bff85b8988590a1b4b6104
Casual-IQ <cyx2582205445@gmail.com>
2024-11-02 17:38:06 +0800

    add necessary library and setup

--- Robot/Inc/imu_task.h
+++ Robot/Inc/imu_task.h
@@ -1,1 +1,53 @@
+#ifndef IMU_TASK_H
+#define IMU_TASK_H
+
+#include "stdint.h"
 #include "cmsis_os.h"
+#include "QuaternionEKF.h"
+
+#define X 0
+#define Y 1
+#define Z 2
+
+#define INS_TASK_PERIOD 1
+
+typedef struct
+{
+    uint8_t flag;
+
+    float scale[3];
+
+    float Yaw;
+    float Pitch;
+    float Roll;
+} IMU_Param_t;
+
+typedef struct
+{
+    float q[4]; // 四元数估计值
+
+    float Gyro[3];  // 角速度
+    float Accel[3]; // 加速度
+    float MotionAccel_b[3]; // 机体坐标加速度
+    float MotionAccel_n[3]; // 绝对系加速度
+
+    float AccelLPF; // 加速度低通滤波系数
+
+    // 加速度在绝对系的向量表示
+    float xn[3];
+    float yn[3];
+    float zn[3];
+
+    float atanxz;
+    float atanyz;
+
+    // 位姿
+    float Roll;
+    float Pitch;
+    float Yaw;
+    float YawTotalAngle;
+} INS_t;
+
+void EKF(void const * argument);
+
+#endif
--- Robot/Src/imu_task.c
+++ Robot/Src/imu_task.c
@@ -2,9 +2,17 @@
 #include "BMI088Middleware.h"
 #include "BMI088driver.h"
 
-fp32 gyro[3], accel[3], temp;
+float gyro[3] = {0, 0, 0};
+float accel[3] = {0, 0, 0};
+float temp = 0;
 
-void EKF(void const * argument){
+const float xb[3] = {1, 0, 0};
+const float yb[3] = {0, 1, 0};
+const float zb[3] = {0, 0, 1};
+
+static void IMU_Param_Correction(IMU_Param_t *param, float gyro[3], float accel[3]);
+
+void INS_task(void const * argument){
     while(BMI088_init())
     {
         ;
@@ -16,3 +24,74 @@ void EKF(void const * argument){
     }
     
 }
+
+/**
+ * @brief reserved.用于修正IMU安装误差与标度因数误差,即陀螺仪轴和云台轴的安装偏移
+ *
+ *
+ * @param param IMU参数
+ * @param gyro  角速度
+ * @param accel 加速度
+ */
+static void IMU_Param_Correction(IMU_Param_t *param, float gyro[3], float accel[3])
+{
+    static float lastYawOffset, lastPitchOffset, lastRollOffset;
+    static float c_11, c_12, c_13, c_21, c_22, c_23, c_31, c_32, c_33;
+    float cosPitch, cosYaw, cosRoll, sinPitch, sinYaw, sinRoll;
+
+    if (fabsf(param->Yaw - lastYawOffset) > 0.001f ||
+        fabsf(param->Pitch - lastPitchOffset) > 0.001f ||
+        fabsf(param->Roll - lastRollOffset) > 0.001f || param->flag)
+    {
+        cosYaw = arm_cos_f32(param->Yaw / 57.295779513f);
+        cosPitch = arm_cos_f32(param->Pitch / 57.295779513f);
+        cosRoll = arm_cos_f32(param->Roll / 57.295779513f);
+        sinYaw = arm_sin_f32(param->Yaw / 57.295779513f);
+        sinPitch = arm_sin_f32(param->Pitch / 57.295779513f);
+        sinRoll = arm_sin_f32(param->Roll / 57.295779513f);
+
+        // 1.yaw(alpha) 2.pitch(beta) 3.roll(gamma)
+        c_11 = cosYaw * cosRoll + sinYaw * sinPitch * sinRoll;
+        c_12 = cosPitch * sinYaw;
+        c_13 = cosYaw * sinRoll - cosRoll * sinYaw * sinPitch;
+        c_21 = cosYaw * sinPitch * sinRoll - cosRoll * sinYaw;
+        c_22 = cosYaw * cosPitch;
+        c_23 = -sinYaw * sinRoll - cosYaw * cosRoll * sinPitch;
+        c_31 = -cosPitch * sinRoll;
+        c_32 = sinPitch;
+        c_33 = cosPitch * cosRoll;
+        param->flag = 0;
+    }
+    float gyro_temp[3];
+    for (uint8_t i = 0; i < 3; i++)
+        gyro_temp[i] = gyro[i] * param->scale[i];
+
+    gyro[X] = c_11 * gyro_temp[X] +
+              c_12 * gyro_temp[Y] +
+              c_13 * gyro_temp[Z];
+    gyro[Y] = c_21 * gyro_temp[X] +
+              c_22 * gyro_temp[Y] +
+              c_23 * gyro_temp[Z];
+    gyro[Z] = c_31 * gyro_temp[X] +
+              c_32 * gyro_temp[Y] +
+              c_33 * gyro_temp[Z];
+
+    float accel_temp[3];
+    for (uint8_t i = 0; i < 3; i++)
+        accel_temp[i] = accel[i];
+
+    accel[X] = c_11 * accel_temp[X] +
+               c_12 * accel_temp[Y] +
+               c_13 * accel_temp[Z];
+    accel[Y] = c_21 * accel_temp[X] +
+               c_22 * accel_temp[Y] +
+               c_23 * accel_temp[Z];
+    accel[Z] = c_31 * accel_temp[X] +
+               c_32 * accel_temp[Y] +
+               c_33 * accel_temp[Z];
+
+    lastYawOffset = param->Yaw;
+    lastPitchOffset = param->Pitch;
+    lastRollOffset = param->Roll;
+}
+
